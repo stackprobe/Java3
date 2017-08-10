0ac970aaa7646ae56da8bcb896b0478c7e0646bd
stackprobe <stackprobes@gmail.com>
2017-08-10 12:21:24 +0900

    t

---
+++ _bat/copy_busy_dlg.bat
@@ -0,0 +1,7 @@
+CD /D C:\pleiades\workspace\Test03\src-03\_bat
+IF NOT EXIST _Test03_bat GOTO END
+
+COPY C:\Dev\Annex\Junk\Labo_old_cs\dunois\BusyDlg\BusyDlg\bin\Release\BusyDlg.exe ..\evergarden\busydlg\res\BusyDlg.exe_
+
+:END
+PAUSE
