397dcc4ceb7802c1c766071763599019d71d2ac0
stackprobe <stackprobes@gmail.com>
2017-09-19 10:58:19 +0900

    daily

--- aqours/codeiq/c3299/Main.java
+++ aqours/codeiq/c3299/Main.java
@@ -1,155 +1,154 @@
-//// _git source mask md5:43206a22f9f6969587a3d6a52619a51e ////
-/////// ////////////////////
+package aqours.codeiq.c3299;
 
-////// ////////////////////
-////// //////////////////
-////// ///////////////
-////// //////////////////
-////// //////////////
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
-////// ///// //// /
-	////// ////// //// ///////////// ///// /
-		/// /
-			/// ///////////////
-		/
-		/////////////// // /
-			////////////////////
-		/
-		///////////////
-	/
+public class Main {
+	public static void main(String[] args) {
+		try {
+			new Main().main2();
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
 
-	/////// ///// ///// /
-		////// //////// ////// / /// //////////
+	private class State {
+		public String[] towers = new String[3];
 
-		////// /////// ///////////// /
-			////// /////////////////// // //////////////////// // /////////////////////
-		/
+		public boolean isCompleted() {
+			return towers[0].isEmpty() && (towers[1].isEmpty() || towers[2].isEmpty());
+		}
 
-		////// ////// /////////// /
-			////// //////////////// ////////
-		/
+		public String toLiteral() {
+			return String.join(":", towers);
+		}
 
-		//////// ///// //// / /////
-	/
+		//public State back = null;
+	}
 
-	/////// /////////// ///// / /// ///////////////////
-	/////// /////////// ///// / /// ///////////////////
+	private List<State> currs = new ArrayList<State>();
+	private List<State> nexts = new ArrayList<State>();
 
-	/////// /////////// ////// / /// //////////////////
+	private Set<String> knowns = new HashSet<String>();
 
-	/////// //// /////// /
-		/////////// // / /// /////////////////// /
-			////// //// / //////////////
+	private void main2() {
+		try(Scanner sc = new Scanner(System.in)) {
+			String line = sc.nextLine();
 
-			///// / / /// ////////
-			/////////// / /////
-			/////////// / ///
-			/////////// / ///
-			/////////////
-			//////////////////////////
+			State s = new State();
+			s.towers[0] = line;
+			s.towers[1] = "";
+			s.towers[2] = "";
+			currs.add(s);
+			knowns.add(s.toLiteral());
 
-			/// /// / /////////
+			int ans = getAns();
 
-			///////////////////// / /////
-		/
-	/
+			System.out.println("" + ans);
+		}
+	}
 
-	/////// /// //////// /
-		/////// /// / // / ////// /
-			///////// //// / ////// /
-				/////// / / // / / // //// /
-					/////// / / // / / // //// /
-						//// // // /
-							/////////////////// // /// /
-								////// ////
-							/
-						/
-					/
-				/
-			/
+	private int getAns() {
+		for(int ans = 1; ; ans++) {
+			for(State curr : currs) {
+				for(int i = 0; i < 3; i++) {
+					for(int j = 0; j < 3; j++) {
+						if(i != j) {
+							if(tryPutNext(curr, i, j)) {
+								return ans;
+							}
+						}
+					}
+				}
+			}
 
-			/////////// /////// / //////
-			///// / //////
-			///// / ////////
-			//////////////
-		/
-	/
+			List<State> bkCurrs = currs;
+			currs = nexts;
+			nexts = bkCurrs;
+			nexts.clear();
+		}
+	}
 
-	///
-	 /
-	 / ////// ////
-	 / ////// /
-	 / ////// /
-	 / /////// // /////////
-	 //
-	/////// /////// //////////////// ///// /// // /// // /
-		//////////////////////////// /
-			////// //////
-		/
-		//// //// / /////////////////////////
+	/**
+	 *
+	 * @param curr
+	 * @param i
+	 * @param j
+	 * @return is completed
+	 */
+	private boolean tryPutNext(State curr, int i, int j) {
+		if(curr.towers[i].isEmpty()) {
+			return false;
+		}
+		char disk = curr.towers[i].charAt(0);
 
-		/////////////// /////////////// // ////// /
-			////// //////
-		/
-		///// //// / /// ////////
+		if(canPut(disk, curr.towers[j]) == false) {
+			return false;
+		}
+		State next = new State();
 
-		/////// / / // / / // //// /
-			////////////// / ///////////////
-		/
-		////////////// / ////////////////////////////
-		////////////// / //// / ///////////////
-		/////////// / /////
+		for(int c = 0; c < 3; c++) {
+			next.towers[c] = curr.towers[c];
+		}
+		next.towers[i] = next.towers[i].substring(1);
+		next.towers[j] = disk + next.towers[j];
+		//next.back = curr;
 
-		////////////////////// /
-			///////////////////
-			////// /////
-		/
-		////// /////// / /////////////////
+		if(next.isCompleted()) {
+			//printRoute(next);
+			return true;
+		}
+		String nextLit = next.toLiteral();
 
-		//////////////////////////// /
-			////// //////
-		/
-		////////////////////
-		////////////////
-		////// //////
-	/
+		if(knowns.contains(nextLit)) {
+			return false;
+		}
+		knowns.add(nextLit);
+		nexts.add(next);
+		return false;
+	}
 
-	/////// /////// /////////// ///// ////// ////// /
-		/////// / / // / / /////////////// //// /
-			////////////////// / ///// /
-				/////// / / // / / // //// /
-					////////////////// / //////////////// /
-						////// //////
-					/
-				/
-			/
-		/
-		////// /////
-	/
+	private boolean canPut(char disk, String tower) {
+		for(int i = 0; i < tower.length(); i++) {
+			if(tower.charAt(i) < disk) {
+				for(int j = 0; j < i; j++) {
+					if(tower.charAt(i) < tower.charAt(j)) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
 
-	//
-	/////// //// //////////////// ////// /
-		// /
-			//////////////////////////////////////
-			///// / ///////////
-		/
-		/////////// // //////
-	/
-	//
-/
+	/*
+	private void printRoute(State state) {
+		do {
+			System.out.println(state.toLiteral());
+			state = state.back;
+		}
+		while(state != null);
+	}
+	*/
+}
 
-///
- /
-////////
+/**
+ *
+testvect
 
-////
-/
+1234
+6
 
-///
-/
+123
+4
 
-////
-/
+1314
+5
 
- /
- //
+ *
+ */
--- aqours/codeiq/c3314/v2/MaskIslands.java
+++ aqours/codeiq/c3314/v2/MaskIslands.java
@@ -1,200 +1,199 @@
-//// _git source mask md5:308b32a881e476a658b258da5c8cc3a5 ////
-/////// ///////////////////////
+package aqours.codeiq.c3314.v2;
 
-////// ////////////////////
-////// /////////////////
-////// ///////////////
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-////// ///////////////////////////
-////// /////////////////////////
-////// ///////////////////////////
+import charlotte.tools.ArrayTools;
+import charlotte.tools.IntTools;
+import charlotte.tools.RunnableEx;
 
-////// ///// /////////// /
-	////// ///////// ///// /
-		////// //// //////////// //////// ////// //////////
-	/
+public class MaskIslands {
+	public interface Found {
+		public void action(int[] islands) throws Exception;
+	}
 
-	////// //// ////////// //// /// //////////////// ///// ////// ////// ///////// /
-		//////// / // ///// ///// // //////
-		//////////////////// / //// ///// ///// // //////
-		////////// // ///// ///// ///// // //////
+	public void search(int div, int islandsTotalMax, Found found) throws Exception {
+		//if(div < 1) throw null; // assert
+		//if(islandsTotalMax < div) throw null; // assert
+		//if(found == null) throw null; // assert
 
-		//// / ////
-		////// / //////
-		//////// / /// /////////
+		_div = div;
+		_found = found;
+		_islands = new int[div];
 
-		///////// /////////////////
+		search(0, islandsTotalMax);
 
-		//// / ///
-		////// / /////
-		//////// / /////
-	/
+		_div = -1;
+		_found = null;
+		_islands = null;
+	}
 
-	/////// /// /////
-	/////// ///// ///////
-	/////// ///// /////////
+	private int _div;
+	private Found _found;
+	private int[] _islands;
 
-	////// //// ////////// ////// /// ///////// ////// ///////// /
-		//////// / ///// /
-			///////////// / // ///// ///// // //////
+	public void search(int index, int spaceRem) throws Exception {
+		if(index < _div) {
+			//if(spaceRem < 1) throw null; // assert
 
-			///// /// ////////// / //////// / ///// / ///// / ///
+			final int ISLAND_MAX = spaceRem - (_div - index - 1);
 
-			/////////////// / // ///// ///// // //////
+			//if(ISLAND_MAX < 1) throw null; // assert
 
-			/////////////////// / // /////////////// // /////////// ////////////////// /
-				//////////// / // //////// / /////////////////
-			/
-		/
-		//// /
-			///////////// / // ///// ///// // //////
+			for(_islands[index] = 1; _islands[index] <= ISLAND_MAX; _islands[index]++) {
+				search(index + 1, spaceRem - _islands[index]);
+			}
+		}
+		else {
+			//if(spaceRem < 0) throw null; // assert
 
-			////////////////////////
-		/
-	/
+			_found.action(_islands);
+		}
+	}
 
-	// //// ////
+	// ---- test
 
-	////// ////// //// ///////////// ///// /
-		/// /
-			/////////
+	public static void main(String[] args) {
+		try {
+			test01();
 
-			//////////////////////////
-		/
-		/////////////// // /
-			////////////////////
-		/
-		///////////////
-	/
+			System.out.println("OK!");
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
 
-	/////// ////// //// //////// /
-		///////// // /// /////// /
-			/// ///// / / //
-			///
-		///////// // /// /////// /
-			/// ///// / / //
-			/// ///// / / //
-			///
-		///////// // /// /////// /
-			/// ///// / / //
-			/// ///// / / //
-			/// ///// / / //
-			///
+	private static void test01() {
+		test01(1, 1, new int[][] {
+			new int[] { 1 },
+			});
+		test01(1, 2, new int[][] {
+			new int[] { 1 },
+			new int[] { 2 },
+			});
+		test01(1, 3, new int[][] {
+			new int[] { 1 },
+			new int[] { 2 },
+			new int[] { 3 },
+			});
 
-		///////// // /// /////// /
-			/// ///// / // / //
-			///
-		///////// // /// /////// /
-			/// ///// / // / //
+		test01(2, 2, new int[][] {
+			new int[] { 1, 1 },
+			});
+		test01(2, 3, new int[][] {
+			new int[] { 1, 1 },
 
-			/// ///// / // / //
-			/// ///// / // / //
-			///
-		///////// // /// /////// /
-			/// ///// / // / //
+			new int[] { 1, 2 },
+			new int[] { 2, 1 },
+			});
+		test01(2, 4, new int[][] {
+			new int[] { 1, 1 },
 
-			/// ///// / // / //
-			/// ///// / // / //
+			new int[] { 1, 2 },
+			new int[] { 2, 1 },
 
-			/// ///// / // / //
-			/// ///// / // / //
-			/// ///// / // / //
-			///
+			new int[] { 1, 3 },
+			new int[] { 2, 2 },
+			new int[] { 3, 1 },
+			});
 
-		///////// // /// /////// /
-			/// ///// / // // / //
-			///
-		///////// // /// /////// /
-			/// ///// / // // / //
+		test01(3, 3, new int[][] {
+			new int[] { 1, 1, 1 },
+			});
+		test01(3, 4, new int[][] {
+			new int[] { 1, 1, 1 },
 
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
-			///
-		///////// // /// /////// /
-			// /
-			/// ///// / // // / //
+			new int[] { 1, 1, 2 },
+			new int[] { 1, 2, 1 },
+			new int[] { 2, 1, 1 },
+			});
+		test01(3, 5, new int[][] {
+			// 3
+			new int[] { 1, 1, 1 },
 
-			// /
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			// 4
+			new int[] { 1, 1, 2 },
+			new int[] { 1, 2, 1 },
+			new int[] { 2, 1, 1 },
 
-			// /
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			// 5
+			new int[] { 1, 1, 3 },
+			new int[] { 1, 3, 1 },
+			new int[] { 3, 1, 1 },
 
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
-			///
-		///////// // /// /////// /
-			// /
-			/// ///// / // // / //
+			new int[] { 1, 2, 2 },
+			new int[] { 2, 1, 2 },
+			new int[] { 2, 2, 1 },
+			});
+		test01(3, 6, new int[][] {
+			// 3
+			new int[] { 1, 1, 1 },
 
-			// /
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			// 4
+			new int[] { 1, 1, 2 },
+			new int[] { 1, 2, 1 },
+			new int[] { 2, 1, 1 },
 
-			// /
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			// 5
+			new int[] { 1, 1, 3 },
+			new int[] { 1, 3, 1 },
+			new int[] { 3, 1, 1 },
 
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			new int[] { 1, 2, 2 },
+			new int[] { 2, 1, 2 },
+			new int[] { 2, 2, 1 },
 
-			// /
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			// 6
+			new int[] { 1, 1, 4 },
+			new int[] { 1, 4, 1 },
+			new int[] { 4, 1, 1 },
 
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
-			/// ///// / // // / //
+			new int[] { 1, 2, 3 },
+			new int[] { 1, 3, 2 },
+			new int[] { 2, 1, 3 },
+			new int[] { 3, 1, 2 },
+			new int[] { 2, 3, 1 },
+			new int[] { 3, 2, 1 },
 
-			/// ///// / // // / //
-			///
-	/
+			new int[] { 2, 2, 2 },
+			});
+	}
 
-	/////// ////// //// ////////// //// /// //////////////// /////// ////////// /
-		/// /
-			/////////// ////// / /////////////////////////
-			/////////// ////// / /// ///////////////////
+	private static void test01(int div, int islandsTotalMax, int[][] prmExpect) {
+		try {
+			List<int[]> expect = Arrays.asList(prmExpect);
+			List<int[]> answer = new ArrayList<int[]>();
 
-			/// ///////////////////////// ////////////////
-					///////// // ///////////////////////////////// ////////////////
-					//
+			new MaskIslands().search(div, islandsTotalMax,
+					(islands) -> answer.add(Arrays.copyOf(islands, islands.length))
+					);
 
-			////////////////////
-			////////////////////
+			test01_sort(expect);
+			test01_sort(answer);
 
-			/////////////////////////////
-					///////
-					///////
-					/// // // /////////////////////////
-							/////////////////////
-							/////////////////////
-							//// /// // // / //
-							/
-					// /
-				///// /////
-			/
-		/
-		/////////////// // /
-			///// /////////////////
-		/
-	/
+			if(!ArrayTools.<int[]>isSame(
+					expect,
+					answer,
+					(a, b) -> ArrayTools.<Integer>comp(
+							ArrayTools.toList(a),
+							ArrayTools.toList(b),
+							(aa, bb) -> aa - bb
+							)
+					)) {
+				throw null;
+			}
+		}
+		catch(Throwable e) {
+			throw RunnableEx.re(e);
+		}
+	}
 
-	/////// ////// //// /////////////////////// ///// /
-		///////////////////// /// // // /////////////////////////
-				/////////////////////
-				///////////////////// ////////////////
-	/
-/
+	private static void test01_sort(List<int[]> list) {
+		ArrayTools.sort(list, (a, b) -> ArrayTools.<Integer>comp(
+				ArrayTools.toList(a),
+				ArrayTools.toList(b), IntTools.comp));
+	}
+}
